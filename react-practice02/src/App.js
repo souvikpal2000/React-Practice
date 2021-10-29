77ed85f029c15ae72410d0c0d55a243cc3c3596e
Souvik Pal <spal62588@gmail.com>
2021-10-29 20:50:09 +0530

    Just for Practice

---
+++ react-practice02/src/App.js
@@ -0,0 +1,21 @@
+const style = {
+  marginLeft:"20px",
+}
+
+function App() {
+  return (
+    <div style={style}>
+      <h1>Web Series Name</h1>
+      <ul>
+        <li>Breaking Bad</li>
+        <li>Prison Break</li>
+        <li>LOST</li>
+        <li>Money Heist</li>
+        <li>Sherlock</li>
+        <li>Mr Robot</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
