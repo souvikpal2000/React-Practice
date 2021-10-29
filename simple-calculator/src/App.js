77c2fc5b9a2af2b2e0c24a1ed23a1daad3d922bc
Souvik Pal <spal62588@gmail.com>
2021-10-29 20:49:22 +0530

    Revision Done

--- simple-calculator/src/App.js
+++ simple-calculator/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { add, sub, mul, div } from './calculation';
+//import { add, sub, mul, div } from './calculation';
+import * as calc from './calculation';
 
 const calculator = () => {
     return (
         <React.Fragment>
             <ul>
-                <li> Summation         : {add(5,2)}</li>
-                <li> Subraction        : {sub(5,2)}</li>
-                <li> Multiplication    : {mul(5,2)}</li>
-                <li> Division          : {div(9,11)}</li>
+                <li> Summation         : {calc.add(5,2)}</li>
+                <li> Subraction        : {calc.sub(5,2)}</li>
+                <li> Multiplication    : {calc.mul(5,2)}</li>
+                <li> Division          : {calc.div(9,11)}</li>
             </ul>
         </React.Fragment>
     );
