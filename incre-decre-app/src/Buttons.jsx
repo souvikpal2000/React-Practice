adceead6804288ec40fd23c17a76e34d25c3c995
Souvik Pal <spal62588@gmail.com>
2021-11-13 20:53:13 +0530

    Made some changes in CSS

--- incre-decre-app/src/Buttons.jsx
+++ incre-decre-app/src/Buttons.jsx
@@ -3,24 +3,13 @@ import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 
-const style = [
-    {
-        backgroundColor: '#12824C',
-        color: 'white'
-    },
-    {
-        backgroundColor: 'red',
-        color: 'white'
-    }
-];
-
 const Buttons = (props) => {
     let {increFunc, decreFunc} = props;
     return(
         <React.Fragment>
             <div className="buttons">
-                <Button className="addBtn" onClick={increFunc} style={style[0]}><AddIcon /></Button>
-                <Button className="minusBtn" onClick={decreFunc} style={style[1]}><RemoveIcon /></Button>
+                <Button className="addBtn" onClick={increFunc}><AddIcon className="icon"/></Button>
+                <Button className="minusBtn" onClick={decreFunc}><RemoveIcon className="icon"/></Button>
             </div>
         </React.Fragment>
     )
