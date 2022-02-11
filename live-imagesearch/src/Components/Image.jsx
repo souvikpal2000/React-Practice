bca124b0129f0e339b45892fc7b9a7ce7675bccb
Souvik Pal <spal62588@gmail.com>
2022-02-11 09:16:49 +0530

    Search Functionality Fixed

--- live-imagesearch/src/Components/Image.jsx
+++ live-imagesearch/src/Components/Image.jsx
@@ -1,25 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-window.img = "";
-
 const Image = (props) => {
-    const {image} = props;
+    const {image, random} = props;
     const client_id = "4mB0CC1xdwTfTQGjF1v1uO9vS2Z8ubzBPd4X0B86IEU"; 
     const fetchUrl = `https://api.unsplash.com/search/photos?client_id=${client_id}&query=${image}`;
-    const random = Math.floor(Math.random()*10);
+    let [img, setImg] = useState("");
     (async function(){
         const res = await axios.get(fetchUrl);
-        if((res.data.results).length !== 0){
-            console.log(res.data.results[random].urls.small_s3);
-            window.img = res.data.results[random].urls.small_s3;
+        if((res.data.results).length !== 0){  
+            const url = res.data.results[random].urls.small_s3;
+            setImg(url);
         }
     })();
     
     return(
         <>
             <div className="imgContainer">
-                {image !== "" ? <img src={window.img} alt={image} /> : null}
+                {image !== "" ? <img src={img} alt={image} /> : null}
             </div>
         </>
     )
--- live-imagesearch/src/Components/Search.jsx
+++ live-imagesearch/src/Components/Search.jsx
@@ -3,18 +3,24 @@ import React, { useState } from "react";
 import Image from "./Image";
 
 const Search = () => {
-    const [text, setText] = useState("");
+    const [data, setData] = useState({
+        text: "",
+        random: 0
+    });
     const setInput = (event) => {
         const value = event.target.value;
-        setText(value);
-        console.log(value);
+        const random = Math.floor(Math.random()*10);
+        setData({
+            text: value,
+            random: random
+        });
     }
     return(
         <>
             <div className="inputContainer">
-                <input type="text" value={text} placeholder="Type Something" onChange={setInput}/>
+                <input type="text" value={data.text} placeholder="Type Something" onChange={setInput}/>
             </div>
-            <Image image={text} />
+            <Image image={data.text} random={data.random} />
         </>
     )
 }
