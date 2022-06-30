d1fa2a3aba0c72d822990cf5fda710718c932c18
souvikpal2000 <spal62588@gmail.com>
2022-06-30 11:52:27 +0530

    Some Feature Added

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 
 const App = () => {
-    const [name,setName] = useState({
+    const [info,setInfo] = useState({
         fname: "",
         lname: "",
         fullName: "",
+        email: ""
     });
     const change = (event) => {
         const { name, value } = event.target;
-        setName((preValue) => {
+        setInfo((preValue) => {
             return{
                 ...preValue,
                 [name]: value
@@ -17,21 +18,24 @@ const App = () => {
     }
     const submit = (event) => {
         event.preventDefault();
-        setName((preValue) => {
+        setInfo((preValue) => {
             return{
                 fname: "",
                 lname: "",
-                fullName: preValue.fname + " " + preValue.lname
+                fullName: preValue.fname + " " + preValue.lname,
+                email: preValue.email
             }
         });
     }
     return(
         <React.Fragment>
             <div className="container">
-                <h1>Hello!! <i>{name.fullName}</i></h1>
+                <h1>Hello!! <i>{info.fullName}</i></h1>
+                <h3>{info.email}</h3>
                 <form onSubmit={submit}>
-                    <input type="text" name="fname" placeholder="Enter First Name" value={name.fname} onChange={change}/>
-                    <input type="text" name="lname" placeholder="Enter Last Name" value={name.lname} onChange={change}/>
+                    <input type="text" name="fname" placeholder="Enter First Name" value={info.fname} onChange={change}/>
+                    <input type="text" name="lname" placeholder="Enter Last Name" value={info.lname} onChange={change}/>
+                    <input type="email" name="email" placeholder="Enter Email" value={info.email} onChange={change}/>
                     <button>Submit</button>
                 </form>
             </div>
