a076e91de44e67265caf5decbb4820a1feb616d1
souvikpal2000 <spal62588@gmail.com>
2022-07-19 09:46:40 +0530

    Simplified

--- notes-app/src/components/Form.jsx
+++ notes-app/src/components/Form.jsx
@@ -4,12 +4,12 @@ const Form = (props) => {
     let {isOpen, values, onChangeFunc, onClickFunc, onSubmitEditFunc, onSubmitFunc, isClose, onCloseFunc} = props;
     return(
         <React.Fragment>
-                <form method="POST" onSubmit={(isOpen.edit && onSubmitEditFunc) || onSubmitFunc}>
-                    {isOpen.active && <input name="title" placeholder='Title' value={values.title} onChange={onChangeFunc}/>}
-                    <input name="note" placeholder='Take Note...' value={values.note} onClick={onClickFunc} onChange={onChangeFunc}/>
-                    {(isOpen.active && isOpen.edit && <input type="submit" value={isOpen.btnValue} />) || (isOpen.active && <input type="submit" value={isOpen.btnValue} />)}
-                </form>
-                {(isClose && <input type="submit" value="Close" onClick={onCloseFunc}/>)}
+            <form method="POST" onSubmit={isOpen.edit? onSubmitEditFunc : onSubmitFunc}>
+                {isOpen.active && <input name="title" placeholder='Title' value={values.title} onChange={onChangeFunc}/>}
+                <input name="note" placeholder='Take Note...' value={values.note} onClick={onClickFunc} onChange={onChangeFunc}/>
+                {(isOpen.active && <input type="submit" value={isOpen.btnValue} />)}
+            </form>
+            {(isClose && <input type="submit" value="Close" onClick={onCloseFunc}/>)}
         </React.Fragment>
     )
 }
