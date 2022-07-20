6155673564e1b88c827f5a5f505a0758eb4d90c2
souvikpal2000 <spal62588@gmail.com>
2022-07-20 12:14:52 +0530

    Learned New Techniques

--- hook-useeffect/src/App.jsx
+++ hook-useeffect/src/App.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect, useState } from "react";
 
 const App = () => {
-    let [count, setCount] = useState(0);
+    let [titleCount, setTitleCount] = useState(0);
+    let [headingCount, setHeadingCount] = useState(0); 
+
+    const increTitleCount = () => {
+        setTitleCount(titleCount+1);
+    }
     useEffect(() => {
-        document.title = `Clicked me ${count}`;
-    })
+        document.title = `Clicked me ${titleCount}`;
+    }, [titleCount]);
+
+    const increHeadingCount = () => {
+        setHeadingCount(headingCount+1);
+    }
+    useEffect(() => {
+        alert(`Heading Count = ${headingCount}`);
+    }, [headingCount]);
+
     return(
         <React.Fragment>
-            <button onClick={() => {
-                setCount(count+1);
-            }}>
-                Click Me {count}
-            </button>
+            <h1>{headingCount}</h1>
+            <button onClick={increTitleCount}>Click Me {titleCount} </button>
+            <button onClick={increHeadingCount}>Click Me {headingCount}</button>
         </React.Fragment>
     )
 }
